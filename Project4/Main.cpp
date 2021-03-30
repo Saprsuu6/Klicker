59b162c4d7d580fec160e46dd6db7f5c67f3e9aa
Andry Saprigin <coffeei.2002@gmail.com>
2021-03-30 20:10:45 +0300

    Clear all

--- Project4/Main.cpp
+++ Project4/Main.cpp
@@ -1,12 +1,13 @@
 #include"Header.h"
 
 int main() {
-	COUNT count{ 0,0,0,0,0 };
+	/*COUNT count{ 0,0,0,0,0 };
 	UPGRADES up{ 0,0,1,nullptr,false };
 	UPGRADES_II up_ii{ false,false,0,0 };
 	Instruction();
 	Frame();
 	Text();
 	Streams(count, up, up_ii);
-	system("pause > NULL");
+	system("pause > NULL");*/
+	cout << "hello world." << endl;
 }
